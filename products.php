14c079305225fbfc5a75515e08c86ab79ce6055f
aaxelr <antonio.axelsson@gmail.com>
2021-05-05 16:21:45 +0200

    add products.php with instances of Product

---
+++ products.php
@@ -0,0 +1,146 @@
+<?php
+require_once "Product.php";
+
+// Create product instances
+$products = array(
+    new Product(
+        1,
+        "Cool Hat",
+        "Wear it everywhere, everyday, no matter what the occasion!",
+        179.90,
+        "Accessories"
+    ),
+    new Product(
+        2,
+        "Cool Beans",
+        "Eat them anywhere, everyday, no matter what the occasion!",
+        19.90,
+        "Food"
+    ),
+    new Product(
+        3,
+        "Cool Cat",
+        "Feed it anywhere, everyday, no matter what the occasion!",
+        999.90,
+        "Pet"
+    ),
+    new Product(
+        4,
+        "Cat Cap",
+        "Wear it only on special occasions!",
+        79.90,
+        "Accessories"
+    ),
+    new Product(
+        5,
+        "Hot Beans",
+        "Eat them before they get cold!",
+        19.90,
+        "Food"
+    ),
+    new Product(
+        6,
+        "Cat Beans",
+        "They look like cats but taste like beans!",
+        17.90,
+        "Food"
+    ),
+    new Product(
+        7,
+        "Bean Cat",
+        "It looks like a kidney bean but is actually a cat :(",
+        599.90,
+        "Pet"
+    ),
+    new Product(
+        8,
+        "Cool Beans Necklace",
+        "Can't eat them anyywhere, everyday, no matter what the occasion, because they aren't real beans!",
+        259.90,
+        "Jewelry"
+    ),
+    new Product(
+        9,
+        "No Beans",
+        "The ultimate luxury accessory. Looks just like beans but invisible!",
+        3999.90,
+        "Jewelry"
+    ),
+    new Product(
+        10,
+        "Hot dog",
+        "Eat them only if you're really hungry...",
+        7.90,
+        "Food"
+    ),
+    new Product(
+        11,
+        "A 'Hot' Dog",
+        "Not food! DO NOT EAT!",
+        399.90,
+        "Pet"
+    ),
+    new Product(
+        12,
+        "Hot Dog Hat",
+        "Hat made out of hot dogs (the food item, not the pet!). Only for special occasions.",
+        29.90,
+        "Accessories"
+    ),
+    new Product(
+        13,
+        "Carlos' Caviar",
+        "First class sturgeon roe, only for special occasions!",
+        319.90,
+        "Food"
+    ),
+    new Product(
+        14,
+        "A fine loaf of bread",
+        "Great bread for everyday use.",
+        5.90,
+        "Food"
+    ),
+    new Product(
+        15,
+        "Pet Squid",
+        "Very cute and loyal pet friend.",
+        129.90,
+        "Pet"
+    ),
+    new Product(
+        16,
+        "Squid Ink Tattoo",
+        "Tribal tattoos made of finest squid ink. Wear them everyday, no matter what the occasion! Remove with soap and warm water.",
+        19.90,
+        "Accessories"
+    ),
+    new Product(
+        17,
+        "Caviar Necklace",
+        "Fancy necklace made of tiny freshwater pearls inspired by Carlos' Caviar.",
+        849.90,
+        "Jewelry"
+    ),
+    new Product(
+        18,
+        "Pet Spider",
+        "Friendly spider, doesn't bite",
+        129.90,
+        "Pet"
+    ),
+    new Product(
+        19,
+        "Spider Wig",
+        "Wig for pet spiders",
+        49.90,
+        "Accessories"
+    ),
+    new Product(
+        20,
+        "Spider Ring",
+        "Spider-shaped ring for special occasions!",
+        19.90,
+        "Jewelry"
+    )
+);
